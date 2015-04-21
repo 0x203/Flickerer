9204cc7fc486c27233dd71e2a219b6ab4abd887f
Cornelius Bock <zweihundertdrei@gmail.com>
2015-04-21 14:12:32 +0200

    add some controls that can be hidden

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-$('document').ready(function(){
-    $('body').click(function() {
-        $(this).toggleClass('black');
+(function () {
+
+    var transmitData;
+    var transmitDuration;
+
+    $('document').ready(function(){
+        $('body').click(function() {
+            //$(this).toggleClass('black');
+            $('.controls').toggleClass('hidden');
+        });
+
+        $('.controls').click(function(e) {
+            // do not hide controls when they are clicked directly
+            e.stopPropagation();
+        });
+
+        $('#transmit_data').on('change', function() {
+            transmitData = $('this').val();
+        });
+
+        $('#transmit_duration').on('change', function() {
+            transmitDuration = $(this).val();
+            $('output[for=transmit_duration]').val(transmitDuration);
+        });
     });
-});
+
+})();
