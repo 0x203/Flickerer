1faeac18c82671515da5ed135a8e61f0912d0789
Cornelius Bock <zweihundertdrei@gmail.com>
2015-04-21 14:33:25 +0200

    enable flickering

--- index.js
+++ index.js
@@ -2,26 +2,43 @@
 
     var transmitData;
     var transmitDuration;
+    var index;
+    var timeoutId;
+
+    var nextBit = function() {
+        var truthy = transmitData[index] === "1";
+        index = (index + 1) % transmitData.length;
+
+        $('body').toggleClass('black', truthy);
+
+        timeoutId = setTimeout(nextBit, transmitDuration);
+    };
+
+    var startTransmission = function() {
+        if(timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        index = 0;
+        nextBit();
+    };
 
     $('document').ready(function(){
         $('body').click(function() {
-            //$(this).toggleClass('black');
             $('.controls').toggleClass('hidden');
         });
 
-        $('.controls').click(function(e) {
-            // do not hide controls when they are clicked directly
-            e.stopPropagation();
-        });
+        $('.controls').on({
+            click: function(e) {
+                // do not hide controls when they are clicked directly
+                e.stopPropagation();
+            },
+            change: function(e) {
+                transmitData = $('#transmit_data').val();
+                transmitDuration = $('#transmit_duration').val();
+                $('output[for=transmit_duration]').val(transmitDuration);
 
-        $('#transmit_data').on('change', function() {
-            transmitData = $('this').val();
-        });
-
-        $('#transmit_duration').on('change', function() {
-            transmitDuration = $(this).val();
-            $('output[for=transmit_duration]').val(transmitDuration);
+                startTransmission();
+            }
         });
     });
-
 })();
