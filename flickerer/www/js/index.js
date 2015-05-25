23d1c345e22938f7d0972847396268a4caea9daa
Cornelius Bock <zweihundertdrei@gmail.com>
2015-05-25 18:13:28 +0200

    write Transmitter class which handles states and timing

--- flickerer/www/js/index.js
+++ flickerer/www/js/index.js
@@ -1,5 +1,100 @@
+Tranmittor = (function() {
+    var Tranmittor = function(inputs, emitBit){
+        this.emitBit = emitBit;     // function to output one bit
+
+        this.bitDuration = inputs.bitDuration;
+        this.initPattern = decodeData(inputs.initializePattern, 'binary');
+        this.initDuration = inputs.initializeDuration * 1000;   // from s to ms
+        this.startPattern = decodeData('11110000', 'binary');  //inputs.startPattern;
+        this.dataWorkload = decodeData(inputs.transmitData, 'binary');
+
+        this.state = STATES.stopped;
+        this.running = false;
+        this.cur_index = null;
+    };
+
+    var STATES = {
+        stopped: 0,
+        running: 1,
+        initializing: 2,
+        starting: 3,
+        transmitting: 4
+    };
+
+    Tranmittor.prototype.start = function() {
+        this.state = STATES.running;
+        this.cur_index = 0;
+        this.tick();
+    };
+
+    Tranmittor.prototype.tick = function() {
+        switch (this.state) {
+            case STATES.stopped:
+                // don't set another timeout
+                return;
+            case STATES.running:
+                this.state = STATES.initializing;
+                this.cur_index = 0;
+                setTimeout(this.initialized.bind(this), this.initDuration);
+                break;
+            case STATES.initializing:
+                this.emitBit(this.initPattern[this.cur_index]);
+                this.cur_index = (this.cur_index + 1) % this.initPattern.length;
+                break;
+            case STATES.starting:
+                this.emitBit(this.startPattern[this.cur_index]);
+                this.cur_index += 1;
+                if(this.cur_index === this.startPattern.length) {
+                    this.cur_index = 0;
+                    this.state = STATES.transmitting;
+                }
+                break;
+            case STATES.transmitting:
+                this.emitBit(this.dataWorkload[this.cur_index]);
+                this.cur_index += 1;
+                if(this.cur_index === this.dataWorkload.length) {
+                    this.cur_index = 0;
+                    this.state = STATES.stopped;
+                }
+                break;
+        }
+
+        this.timeoutId = setTimeout(this.tick.bind(this), this.bitDuration);
+    };
+
+    Tranmittor.prototype.stop = function() {
+        this.state = STATES.stopped;
+    };
+
+    Tranmittor.prototype.initialized = function() {
+        if(this.state === STATES.initializing) {
+            // continue with sending start pattern
+            this.cur_index = 0;
+            this.state = STATES.starting;
+        }
+    };
+
+    var decodeData = function(data, encoding) {
+        var decoded = [];
+        if(encoding === 'binary') {
+            var len = data.length;
+            for(var i = 0; i < len; i++) {
+                decoded.push(data[i] === '1');
+            }
+        } else {
+            console.warn('Implement me!');
+            decoded = [0, 1, 0, 1, 1];
+        }
+        return decoded;
+    };
+
+    return Tranmittor;
+})();
+
+
 (function () {
 
+    var transmitter;
     var inputState = {
         initializePattern: null,
         initializeDuration: null,
@@ -7,16 +102,9 @@
         bitDuration: null,
         paused: false
     };
-    var index;
-    var timeoutId;
 
-    var nextBit = function() {
-        var truthy = inputState.transmitData[index] === '1';
-        index = (index + 1) % inputState.transmitData.length;
-
-        $('body').toggleClass('black', truthy);
-
-        timeoutId = setTimeout(nextBit, inputState.bitDuration);
+    var emitBit = function(bit) {
+        $('body').toggleClass('black', bit);
     };
 
     var uiChange = function() {
@@ -40,11 +128,8 @@
     };
 
     var startTransmission = function() {
-        if(timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        index = 0;
-        nextBit();
+        transmitter = new Tranmittor(inputState, emitBit);
+        transmitter.start();
     };
 
     $('document').ready(function(){
@@ -63,11 +148,9 @@
         $('.pause').on('click', function(e) {
             inputState.paused = !inputState.paused;
             if(inputState.paused) {
-               if(timeoutId) {
-                    clearTimeout(timeoutId);
-                }
+                transmitter.stop();
             } else {
-                startTransmission();
+                transmitter.start();
             }
 
             $(this).toggleClass('paused', inputState.paused);
