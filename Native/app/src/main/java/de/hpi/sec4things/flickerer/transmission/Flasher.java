cea841c1d667c6ed9e2d4b8530dd6b9ff990cb11
Daniel Werner <daniel.werner@student.hpi.uni-potsdam.de>
2015-07-23 11:44:37 +0200

    fix flashlight for nexus 5, speed up turnOn and turnOff, but still very slow

--- Native/app/src/main/java/de/hpi/sec4things/flickerer/transmission/Flasher.java
+++ Native/app/src/main/java/de/hpi/sec4things/flickerer/transmission/Flasher.java
@@ -1,30 +1,42 @@
 package de.hpi.sec4things.flickerer.transmission;
 
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
+
+import java.io.IOException;
 
 public class Flasher implements Emitter{
 
-    private final Camera camera;
-    private final Camera.Parameters p;
+    private static Camera camera;
+    private final Camera.Parameters pOn, pOff;
     private boolean isOn;
 
     public Flasher() {
         camera = Camera.open();
-        p = camera.getParameters();
+
+        pOn = camera.getParameters();
+        pOn.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        pOff = camera.getParameters();
+        pOff.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+
+        // Nexus 5 needs previewTexture
+        try {
+            camera.setPreviewTexture(new SurfaceTexture(0));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        camera.startPreview();
         isOn = false;
     }
     
     private void turnOn() {
-        p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-        camera.setParameters(p);
-        camera.startPreview();
+        camera.setParameters(pOn);
         isOn = true;
     }
 
     private void turnOff() {
-        p.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-        camera.setParameters(p);
-        camera.stopPreview();
+        camera.setParameters(pOff);
         isOn = false;
     }
 
